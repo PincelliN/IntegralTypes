332f0825e89a2425cd08d63991346f3b83875334
Nicolò Pincelli <pincelli.n@gmail.com>
2024-12-11 16:20:15 +0100

    Feat: inizzializzazione delle variabili a,b,c,d e assegnazione del valore

--- Program.cs
+++ Program.cs
@@ -9,3 +9,19 @@ Console.WriteLine("Primo={0}", a);
 //interpolazione di sringhe 
 Console.WriteLine($"primo={a}");
 
+short b = 100;
+
+Console.WriteLine("Secondo={0}", b);
+Console.WriteLine($"secondo={b}");
+
+sbyte c = -50;
+
+Console.WriteLine("Terzo={0}", c);
+Console.WriteLine($"terzo={c}");
+
+long d = 1_000_000;
+
+Console.WriteLine("Quarto={0}", d);
+Console.WriteLine($"quarto={d}");
+
+
