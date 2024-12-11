49f064e7ceb834810b87c14dab831ae8c85171c3
Nicolò Pincelli <pincelli.n@gmail.com>
2024-12-11 16:12:02 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Hello, World!");
+
+int a = 500;
+//stringa di formato
+Console.WriteLine("Primo={0}", a);
+//interpolazione di sringhe 
+Console.WriteLine($"primo={a}");
